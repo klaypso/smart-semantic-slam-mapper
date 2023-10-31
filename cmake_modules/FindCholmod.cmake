07e304f5b15881074f159b0b4aa60f964f208b21
klaypso <klaypso@proton.me>
2023-10-31 23:10:13 +0000

    remove env

--- cmake_modules/FindCholmod.cmake
+++ cmake_modules/FindCholmod.cmake
@@ -1,4 +1,13 @@
 # Cholmod lib usually requires linking to a blas and lapack library.
 # It is up to the user of this module to find a BLAS and link to it.
 
-if (
+if (CHOLMOD_INCLUDE_DIR AND CHOLMOD_LIBRARIES)
+  set(CHOLMOD_FIND_QUIETLY TRUE)
+endif (CHOLMOD_INCLUDE_DIR AND CHOLMOD_LIBRARIES)
+
+find_path(CHOLMOD_INCLUDE_DIR
+  NAMES
+  cholmod.h
+  PATHS
+  $ENV{CHOLMODDIR}
+  ${INC
