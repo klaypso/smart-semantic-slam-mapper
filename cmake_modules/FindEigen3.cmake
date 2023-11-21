4a9534f1ae1860accf93164e32a7f288fea0c5c7
klaypso <klaypso@proton.me>
2023-11-21 22:50:45 +0000

    removed index redirects

--- cmake_modules/FindEigen3.cmake
+++ cmake_modules/FindEigen3.cmake
@@ -55,4 +55,12 @@ endmacro(_eigen3_check_version)
 
 if (EIGEN3_INCLUDE_DIR)
 
-  # in cac
+  # in cache already
+  _eigen3_check_version()
+  set(EIGEN3_FOUND ${EIGEN3_VERSION_OK})
+
+else (EIGEN3_INCLUDE_DIR)
+
+  find_path(EIGEN3_INCLUDE_DIR NAMES signature_of_eigen3_matrix_library
+      PATHS
+      ${CMAKE_INSTALL_PREFIX}/inc
