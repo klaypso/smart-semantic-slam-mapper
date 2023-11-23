db8f1396ba4ac2f49f70af52746f3c360e24d94c
klaypso <klaypso@proton.me>
2023-11-23 07:38:48 +0000

    database fix

--- cmake_modules/FindEigen3.cmake
+++ cmake_modules/FindEigen3.cmake
@@ -63,4 +63,13 @@ else (EIGEN3_INCLUDE_DIR)
 
   find_path(EIGEN3_INCLUDE_DIR NAMES signature_of_eigen3_matrix_library
       PATHS
-      ${CMAKE_INSTALL_PREFIX}/inc
+      ${CMAKE_INSTALL_PREFIX}/include
+      ${KDE4_INCLUDE_DIR}
+      PATH_SUFFIXES eigen3 eigen
+    )
+
+  if(EIGEN3_INCLUDE_DIR)
+    _eigen3_check_version()
+  endif(EIGEN3_INCLUDE_DIR)
+
+  include(FindPackageHandleStandardA
