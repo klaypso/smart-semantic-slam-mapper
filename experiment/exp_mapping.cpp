e8a4e3de76904b51db479bc31f93fd1e14efba38
klaypso <klaypso@proton.me>
2023-12-07 09:41:27 +0000

    Bump version to 0.4.7

--- experiment/exp_mapping.cpp
+++ experiment/exp_mapping.cpp
@@ -2,4 +2,12 @@
 #include "track.h"
 #include "pose_graph.h"
 #include "common_headers.h"
-#includ
+#include "mapper.h"
+#include "readGTPose.h"
+#include "vo_stereo.hpp"
+
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/common/common_headers.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/visualiza
