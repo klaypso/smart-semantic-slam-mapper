9199631091054d169f8ef7acdca6470d4959236d
klaypso <klaypso@proton.me>
2024-01-09 07:47:43 +0000

    Input: aiptek - fix crash on detecting device without endpoints

---
+++ include/basicStructure.hpp
@@ -0,0 +1,7 @@
+#ifndef BASICSTRUCTURE__HPP
+#define BASICSTRUCTURE__HPP
+#include <iostream>
+
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/highgui/highgui.hpp"
