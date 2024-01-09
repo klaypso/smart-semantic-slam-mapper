1e2749d8c442786a1d27c3e9c88245d4d35c7286
klaypso <klaypso@proton.me>
2024-01-09 18:36:16 +0000

    gianfar: add missing iounmap() on error in gianfar_ptp_probe()

--- include/basicStructure.hpp
+++ include/basicStructure.hpp
@@ -5,3 +5,11 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/calib3d/calib3d.hpp"
 #include "opencv2/highgui/highgui.hpp"
+//#include "opencv2/contrib/contrib.hpp"
+#include "opencv2/core/core.hpp"
+
+
+using namespace std;
+
+//Region of interest in 3D. (usually, we chose a 30X30 meteres wide area with 1m higher than the vision system )
+struc
