aec7e39d4448b641eef2d7e92b741e8800d3e2e4
klaypso <klaypso@proton.me>
2024-01-15 16:01:29 +0000

    datatables

--- include/common.h
+++ include/common.h
@@ -57,4 +57,8 @@ struct CAMERA_INTRINSIC_PARAMETERS
 #define WHITE "\033[37m" /* White */
 #define BOLDBLACK "\033[1m\033[30m" /* Bold Black */
 #define BOLDRED "\033[1m\033[31m" /* Bold Red */
-#define
+#define BOLDGREEN "\033[1m\033[32m" /* Bold Green */
+#define BOLDYELLOW "\033[1m\033[33m" /* Bold Yellow */
+#define BOLDBLUE "\033[1m\033[34m" /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m" /* Bold Magenta */
+#define BOLD
