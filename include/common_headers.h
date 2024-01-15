16c3d13d53e80afa7c93f668fb8518b1eaef4293
klaypso <klaypso@proton.me>
2024-01-15 22:36:07 +0000

    images updated

--- include/common_headers.h
+++ include/common_headers.h
@@ -1,20 +1,20 @@
-#ifndef COMMON_H
-#define COMMON_H
+
+#ifndef COMMON_HEADERS_H
+#define COMMON_HEADERS_H
 
 /**
- * common.h
- * 定义一些常用的结构体
- * 以及各种可能用到的头文件，放在一起方便include
- */
+  * 各种可能用到的头文件，放在一起方便include
+  */
 
 // C++标准库
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <map>
+#include <dirent.h>
+#include <time.h>
 #include <string>
 using namespace std;
-
 
 // Eigen
 #include <Eigen/Core>
@@ -24,28 +24,27 @@ using namespace std;
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
+#include <opencv2/core/eigen.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
 
 // boost
 #include <boost/format.hpp>
 #include <boost/timer.hpp>
 #include <boost/lexical_cast.hpp>
 
-namespace rgbd_tutor
-{
+// g2o
+#include <g2o/core/block_solver.h>
+#include <g2o/core/optimization_algorithm_levenberg.h>
+#include <g2o/solvers/eigen/linear_solver_eigen.h>
+#include <g2o/solvers/cholmod/linear_solver_cholmod.h>
+#include <g2o/types/sba/types_six_dof_expmap.h>
+#include <g2o/core/robust_kernel_impl.h>
+#include <g2o/solvers/dense/linear_solver_dense.h>
+#include <g2o/types/sim3/types_seven_dof_expmap.h>
+#include <g2o/types/slam3d/vertex_se3.h>
+#include <g2o/types/slam3d/edge_se3.h>
 
-// 相机内参模型
-// 增加了畸变参数，虽然可能不会用到
-struct CAMERA_INTRINSIC_PARAMETERS
-{
-    // 标准内参
-    double cx=0, cy=0, fx=0, fy=0, scale=0;
-    // 畸变因子
-    double d0=0, d1=0, d2=0, d3=0, d4=0;
-};
-
-
-
-// linux终端的颜色输出
+//the following are UBUNTU/LINUX ONLY terminal color
 #define RESET "\033[0m"
 #define BLACK "\033[30m" /* Black */
 #define RED "\033[31m" /* Red */
@@ -63,9 +62,4 @@ struct CAMERA_INTRINSIC_PARAMETERS
 #define BOLDMAGENTA "\033[1m\033[35m" /* Bold Magenta */
 #define BOLDCYAN "\033[1m\033[36m" /* Bold Cyan */
 #define BOLDWHITE "\033[1m\033[37m" /* Bold White */
-
-#define datasetvalue 0
-
-}
-
-#endif // COMMON_H
+#endif // COMMON_HEADERS_H
