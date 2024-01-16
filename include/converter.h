f0ef3b5f292717da02e805e51b3879524a01c8c7
klaypso <klaypso@proton.me>
2024-01-16 14:25:58 +0000

    Update setup_apt.sh

--- include/converter.h
+++ include/converter.h
@@ -1,1 +1,9 @@
-#ifndef MY_CONVERT
+#ifndef MY_CONVERTER_H
+#define MY_CONVERTER_H
+
+#include "common_headers.h"
+#include "rgbdframe.h"
+#include "Thirdparty/orbslam_modified/include/Converter.h"
+
+/* *
+ * convert the common types in cv, eigen and g2o
