2cce486a691b2cc4ba0eec0049f20c8d5adb3beb
klaypso <klaypso@proton.me>
2024-01-16 17:55:37 +0000

    release version 1.0.0

--- include/converter.h
+++ include/converter.h
@@ -17,4 +17,9 @@ public:
     {
         Eigen::Matrix3d R = T.rotation();
         /*
-  
+        R << T(0,0) << T(0,1) << T(0,2) <<
+             T(1,0) << T(1,1) << T(1,2) <<
+             T(2,0) << T(2,1) << T(2,2);*/
+        Eigen::Vector3d t( T(0,3), T(1,3), T(2,3) );
+
+        return 
