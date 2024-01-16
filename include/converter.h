3d8e0f176b21ac0a214caeff8c744bd00890e29e
klaypso <klaypso@proton.me>
2024-01-16 16:13:17 +0000

    zram: rename struct `table' to `zram_table_entry'

--- include/converter.h
+++ include/converter.h
@@ -6,4 +6,15 @@
 #include "Thirdparty/orbslam_modified/include/Converter.h"
 
 /* *
- * convert the common types in cv, eigen and g2o
+ * convert the common types in cv, eigen and g2o.
+ */
+
+namespace rgbd_tutor {
+class Converter: public ORB_SLAM2::Converter
+{
+public:
+    static  g2o::SE3Quat    toSE3Quat(const Eigen::Isometry3d& T)
+    {
+        Eigen::Matrix3d R = T.rotation();
+        /*
+  
