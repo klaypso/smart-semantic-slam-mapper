280ca4c308c8eddb047e865b4075e2f6d97fb1ea
klaypso <klaypso@proton.me>
2024-01-18 12:21:13 +0000

    Add incremental test.

--- include/feature.h
+++ include/feature.h
@@ -12,4 +12,11 @@ public:
     Feature( ) {}
 public:
     cv::KeyPoint    keypoint;
-    cv::Ma
+    cv::Mat         descriptor;
+    cv::Point3f     position;       // position in 3D space
+    float           observe_frequency = 0.0;      //被观测到的频率
+};
+
+}
+
+#endif // FEATURE_H
---
+++ include/looper.h
@@ -0,0 +1,5 @@
+#ifndef LOOPER_H
+#define LOOPER_H
+
+/**
+  * Looper.h
