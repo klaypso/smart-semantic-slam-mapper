bbf508dcb586085a23beb99873775e0d1a4cf711
klaypso <klaypso@proton.me>
2024-01-18 16:37:52 +0000

    add head

--- include/looper.h
+++ include/looper.h
@@ -3,3 +3,14 @@
 
 /**
   * Looper.h
+  * The loop closure detector based on dbow2;
+  */
+
+#include "common_headers.h"
+#include "rgbdframe.h"
+#include "converter.h"
+
+#include <opencv2/opencv.hpp>
+
+#include "Thirdparty/DBoW2/DBoW2/FORB.h"
+#inc
