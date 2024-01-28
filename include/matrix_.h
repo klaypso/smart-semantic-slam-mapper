ed8c581375813f93dad6aa7f26f93da596fe0f51
klaypso <klaypso@proton.me>
2024-01-28 20:16:34 +0000

    docs tweaks

--- include/matrix_.h
+++ include/matrix_.h
@@ -6,4 +6,8 @@ Karlsruhe Institute of Technology, Germany
 This file is part of libviso2.
 Authors: Andreas Geiger
 
-libviso2 is free softwa
+libviso2 is free software; you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or any later version.
+
+libviso
