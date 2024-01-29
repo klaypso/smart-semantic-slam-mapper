2b97039e3aa7f261e9d4ab753b1fae975cc515f2
klaypso <klaypso@proton.me>
2024-01-29 11:56:18 +0000

    Update release-process.md

--- include/matrix_.h
+++ include/matrix_.h
@@ -10,4 +10,6 @@ libviso2 is free software; you can redistribute it and/or modify it under the
 terms of the GNU General Public License as published by the Free Software
 Foundation; either version 2 of the License, or any later version.
 
-libviso
+libviso2 is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Publi
