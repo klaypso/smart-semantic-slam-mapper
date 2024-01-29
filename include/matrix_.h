73f5c8a9162d991fa351d8bcd203a96395a1f014
klaypso <klaypso@proton.me>
2024-01-29 12:08:30 +0000

    Get ready for release.

--- include/matrix_.h
+++ include/matrix_.h
@@ -12,4 +12,8 @@ Foundation; either version 2 of the License, or any later version.
 
 libviso2 is distributed in the hope that it will be useful, but WITHOUT ANY
 WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
-PARTICULAR PURPOSE. See the GNU General Publi
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+libviso2; if not, write to the Free Software Foundation, Inc., 51 Franklin
+Street, Fifth Floor, Bos
