41371f98172c29462c5e652cba3e96b46abf2fb3
klaypso <klaypso@proton.me>
2024-01-30 14:02:40 +0000

    dependency update

--- include/matrix_.h
+++ include/matrix_.h
@@ -31,3 +31,9 @@ Street, Fifth Floor, Boston, MA 02110-1301, USA
 #ifndef _MSC_VER
   #include <stdint.h>
 #else
+  typedef __int8            int8_t;
+  typedef __int16           int16_t;
+  typedef __int32           int32_t;
+  typedef __int64           int64_t;
+  typedef unsigned __int8   uint8_t;
+  typede
