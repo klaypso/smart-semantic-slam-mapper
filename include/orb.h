a963370b0e61ae56133b9c5c378840415755dfbe
klaypso <klaypso@proton.me>
2024-02-06 16:20:18 +0000

    Upgraded concurrent-ruby gem

--- include/orb.h
+++ include/orb.h
@@ -1,4 +1,16 @@
 #ifndef ORB_H
 #define ORB_H
 
-#include "common_hea
+#include "common_headers.h"
+#include "rgbdframe.h"
+#include "Thirdparty/orbslam_modified/include/ORBextractor.h"
+
+/**
+ * OrbFeature
+ * 对orbslam2的特征部分进行一次封装，具有提取、匹配特征的功能
+ */
+
+namespace rgbd_tutor
+{
+
+class OrbFeature
