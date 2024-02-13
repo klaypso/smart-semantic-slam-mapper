3d70c88c53a71a29022aec2c48910e1ce763b896
klaypso <klaypso@proton.me>
2024-02-13 18:17:08 +0000

    release 0.2.5

--- include/pose_graph.h
+++ include/pose_graph.h
@@ -2,4 +2,17 @@
 #define POSE_GRAPH_H
 #include "common_headers.h"
 #include "parameter_reader.h"
-#include "rgbdframe.h
+#include "rgbdframe.h"
+#include "looper.h"
+#include "pnp.h"
+#include "orb.h"
+#include "track.h"
+
+#include <thread>
+#include <mutex>
+#include <functional>
+#include <map>
+#include <condition_variable>
+
+/**
+  * The pose g
