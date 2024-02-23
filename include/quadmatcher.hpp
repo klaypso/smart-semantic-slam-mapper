a2a7a8a026bbb7af5163be5a1a55a396d41c9573
klaypso <klaypso@proton.me>
2024-02-23 11:34:39 +0000

    3.1.8

--- include/quadmatcher.hpp
+++ include/quadmatcher.hpp
@@ -1,3 +1,12 @@
 /*
    IRTES-SET laboratory
-   Aut
+   Authors: You Li (liyou026@gmail.com)
+   Descirption: This a sample code of my PhD works
+*/
+
+#ifndef QUADMATCHER__HPP
+#define QUADMATCHER__HPP
+
+#include <iostream>
+
+#include "opencv2/imgproc/imgproc.h
