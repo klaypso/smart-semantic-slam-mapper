5bf11e2c5a63c926861b10cd19d77f85764be028
klaypso <klaypso@proton.me>
2024-02-24 09:13:56 +0000

    Update version to 0.2.0

--- include/quadmatcher.hpp
+++ include/quadmatcher.hpp
@@ -9,4 +9,10 @@
 
 #include <iostream>
 
-#include "opencv2/imgproc/imgproc.h
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/contrib/contrib.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/n
