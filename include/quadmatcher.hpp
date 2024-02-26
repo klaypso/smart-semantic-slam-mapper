646a798d8cf9a6315b4796f445e511cac71f1ad3
klaypso <klaypso@proton.me>
2024-02-26 10:27:46 +0000

    serialize

--- include/quadmatcher.hpp
+++ include/quadmatcher.hpp
@@ -43,4 +43,7 @@ struct pmatch {
 
     pmatch(){}
     pmatch(float u1p,float v1p,int32_t i1p,float u2p,float v2p,int32_t i2p,
-            float 
+            float u1c,float v1c,int32_t i1c,float u2c,float v2c,int32_t i2c):
+            u1p(u1p),v1p(v1p),i1p(i1p),u2p(u2p),v2p(v2p),i2p(i2p),
+            u1c(u1c),v1c(v1c),i1c(i1c),u2c(u2c),v2c(v2c),i2c(i2c) {}
+  };
