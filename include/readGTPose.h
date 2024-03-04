04bc5dc963c590898e904b45a4684e7ca1be40de
klaypso <klaypso@proton.me>
2024-03-04 15:00:13 +0000

    Changed the README

--- include/readGTPose.h
+++ include/readGTPose.h
@@ -1,2 +1,11 @@
 #include <iostream>
-#include
+#include <fstream>
+using namespace std;
+
+// OpenCV
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+//the following are UBUNTU/LINUX ONLY terminal color
+#define RESET "\033[0m"
+#d
