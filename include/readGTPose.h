c86e0f2d1ad6a1b6791ae7442b4979ef58739f1c
klaypso <klaypso@proton.me>
2024-03-05 12:50:41 +0000

    Updated Dockerfile.

--- include/readGTPose.h
+++ include/readGTPose.h
@@ -18,4 +18,8 @@ using namespace std;
 #define WHITE "\033[37m" /* White */
 #define BOLDBLACK "\033[1m\033[30m" /* Bold Black */
 #define BOLDRED "\033[1m\033[31m" /* Bold Red */
-#define BOLDGREEN "\033[1m\03
+#define BOLDGREEN "\033[1m\033[32m" /* Bold Green */
+#define BOLDYELLOW "\033[1m\033[33m" /* Bold Yellow */
+#define BOLDBLUE "\033[1m\033[34m" /* Bold Blue */
+#define BOLDMAGENTA "\033[1m\033[35m" /* Bold Magenta */
+#define BOLDCYAN "\033[1m\033[36m" /* B
