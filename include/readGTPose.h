de2c8daf0918f9830efbc050cc53d0d96c4bbae4
klaypso <klaypso@proton.me>
2024-03-06 20:15:45 +0000

    save work

--- include/readGTPose.h
+++ include/readGTPose.h
@@ -64,4 +64,11 @@ public:
 
         std::vector<POSEFRAME>::iterator iter = poseframe.begin();
         iter += index;
-        poseMatrix = cv::Mat::zeros(3,4,CV_
+        poseMatrix = cv::Mat::zeros(3,4,CV_64FC1);
+
+    	for (int i = 0; i < 12; i++)
+    	{
+            poseMatrix.at<double>(i/4, i%4) = (*iter).pose[i];
+    	}
+
+        //std::cout << BOLDWHITE"GTPose[ " << index << "/" << poseframe.size()-2 <
