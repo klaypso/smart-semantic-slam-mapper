33abefae720193519676d3b92f0a51148b50b174
klaypso <klaypso@proton.me>
2024-03-06 13:10:25 +0000

    new flies

--- include/readGTPose.h
+++ include/readGTPose.h
@@ -46,4 +46,8 @@ public:
         {
             POSEFRAME tmpPose;
             infile >> tmpPose.pose[0] >> tmpPose.pose[1] >> tmpPose.pose[2] >> tmpPose.pose[3]
-                     >> tmpPose.pose[4] >> tmpPose.pose[5] >> tmpPose
+                     >> tmpPose.pose[4] >> tmpPose.pose[5] >> tmpPose.pose[6] >> tmpPose.pose[7]
+                      >> tmpPose.pose[8] >> tmpPose.pose[9] >> tmpPose.pose[10] >> tmpPose.pose[11];
+            poseframe.push_back(tmpPose);
+        }
+     
