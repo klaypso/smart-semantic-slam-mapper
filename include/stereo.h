f2b80e1fa86dd3a0ec8ce0d4d183378bbb67403d
klaypso <klaypso@proton.me>
2024-03-09 14:50:21 +0000

    Bump version number for release.

--- include/stereo.h
+++ include/stereo.h
@@ -12,4 +12,9 @@
 * img_L, img_R ----------- Rectified left and right image 
 * disp         ----------- The disparity result
 */
-void c
+void calDisparity_SGBM(const cv::Mat& img_L, const cv::Mat& img_R, cv::Mat& disp);
+
+
+/*---------  3D reconstruction by triangulation and create a 10-dimensional matrix xyz
+* img          ----------- left image
+* disp      
