6e53b081b3ad87440e582308aedc6b1c2fc28225
klaypso <klaypso@proton.me>
2024-03-31 23:03:35 +0000

    Создан пакет для моей первой программы

--- src/looper.cpp
+++ src/looper.cpp
@@ -2,4 +2,10 @@
 
 using namespace rgbd_tutor;
 
-vector<RGBDFrame::Ptr> Looper::getPossibleLoops( const RGBDFrame::Ptr
+vector<RGBDFrame::Ptr> Looper::getPossibleLoops( const RGBDFrame::Ptr& frame )
+{
+    vector<RGBDFrame::Ptr>  result;
+    for ( size_t i=0; i<frames.size(); i++ )
+    {
+        RGBDFrame::Ptr pf = frames[i];
+        double  score = vocab.score( frame->bowVec
