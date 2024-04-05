eecec6456ae7d88b3551039d26aed56744697b0b
klaypso <klaypso@proton.me>
2024-04-05 09:47:38 +0000

    Regenerate gemspec for version 0.0.7

--- src/orb.cpp
+++ src/orb.cpp
@@ -9,4 +9,11 @@
 #include "common_headers.h"
 #include "converter.h"
 #include "orb.h"
-using na
+using namespace std;
+
+using namespace rgbd_tutor;
+
+vector<cv::DMatch> OrbFeature::match( const RGBDFrame::Ptr& frame1, const RGBDFrame::Ptr& frame2 ) const
+{
+    vector< vector<cv::DMatch> > matches_knn;
+    cv::Mat desp1 = frame1->getAllDescriptor
