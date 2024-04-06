eb095e303a1ab1ca898df844cd4c19d20b53ac1c
klaypso <klaypso@proton.me>
2024-04-06 06:34:58 +0000

    Fix missing include.

--- src/parameter_reader.cpp
+++ src/parameter_reader.cpp
@@ -4,4 +4,8 @@
 rgbd_tutor::CAMERA_INTRINSIC_PARAMETERS rgbd_tutor::ParameterReader::getCamera() const
 {
     static rgbd_tutor::CAMERA_INTRINSIC_PARAMETERS camera;
-    camera.fx = this->getData<double>("camera.fx"
+    camera.fx = this->getData<double>("camera.fx");
+    camera.fy = this->getData<double>("camera.fy");
+    camera.cx = this->getData<double>("camera.cx");
+    camera.cy = this->getData<double>("camera.cy");
+    camera.d0 = this-
