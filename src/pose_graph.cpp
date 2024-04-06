4852d512d86b7819e7e1d8d30a825efd918c8c98
klaypso <klaypso@proton.me>
2024-04-06 18:36:57 +0000

    Fix #57

--- src/pose_graph.cpp
+++ src/pose_graph.cpp
@@ -1,3 +1,9 @@
 #include "pose_graph.h"
 #include "utils.h"
-using namespace rgbd_
+using namespace rgbd_tutor;
+
+/**
+ * @brief PoseGraph::tryInsertKeyFrame
+ * @param frame
+ * @return
+ * TODO: Problem: when inserting large number of keyframes, the mapping thread will block for long time, causing even more key-frames to
