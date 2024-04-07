f662e270ee81c96bef898fea0eeb89113871ea48
klaypso <klaypso@proton.me>
2024-04-07 08:16:10 +0000

    Initialize repository.

--- src/pose_graph.cpp
+++ src/pose_graph.cpp
@@ -6,4 +6,12 @@ using namespace rgbd_tutor;
  * @brief PoseGraph::tryInsertKeyFrame
  * @param frame
  * @return
- * TODO: Problem: when inserting large number of keyframes, the mapping thread will block for long time, causing even more key-frames to
+ * TODO: Problem: when inserting large number of keyframes, the mapping thread will block for long time, causing even more key-frames to be processed.
+ */
+bool PoseGraph::tryInsertKeyFrame(RGBDFrame::Ptr& frame)
+{
+    if ( keyframes.size() == 0 )
+    {
+        // 图是空的，直接加入原始点
+        unique_lock<mutex> lck(keyframes_mutex);
+        keyframe
