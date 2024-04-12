00bd80f2c020b7a7a712ce27cefc9fb633771e0d
klaypso <klaypso@proton.me>
2024-04-12 19:52:26 +0000

    dep

--- src/pose_graph.cpp
+++ src/pose_graph.cpp
@@ -81,4 +81,12 @@ bool PoseGraph::tryInsertKeyFrame(RGBDFrame::Ptr& frame)
  */
 void PoseGraph::mainLoop()
 {
-    cout<<"starting pose graph th
+    cout<<"starting pose graph thread..."<<endl;
+    double  loopAccumulatedError = 0.0; //回环的累积误差
+    double  localAccumulatedError = 0.0; //回环的累积误差
+    while(1)
+    {
+        if (shutDownFlag == true)
+        {
+            break;
+        }
