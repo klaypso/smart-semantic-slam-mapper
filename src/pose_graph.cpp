2665525db988f17314a15514e5c16443e472db8c
klaypso <klaypso@proton.me>
2024-04-21 10:33:15 +0000

    ignore config file

--- src/pose_graph.cpp
+++ src/pose_graph.cpp
@@ -187,4 +187,8 @@ void PoseGraph::mainLoop()
                 if ( isEdgeExist( nf->id, pf->id ) ) //这条边已经存在
                     continue;
                 PNP_INFORMATION info;
-                if ( pnp->solvePnPLazy( pf, nf, info, fals
+                if ( pnp->solvePnPLazy( pf, nf, info, false) == true )
+                {
+// viso
+			// 用viso2检测nf和pf之间是否可以计算一个边
+			QuadFeatureMatch* quadmatcher = new QuadFeatureMatch(nf->img_lc,nf->img_rc,pf->img_lc,pf->img_rc,nf->semantic_cur_r,pf->semantic_cu
