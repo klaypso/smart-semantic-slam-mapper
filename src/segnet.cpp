73e4432da74b1daec247e32e14d239ddb7763edc
klaypso <klaypso@proton.me>
2024-05-04 20:30:30 +0000

    refacto

--- src/segnet.cpp
+++ src/segnet.cpp
@@ -12,4 +12,6 @@ Classifier::Classifier()
 #endif
 
     //std::string model_file = "/home/relaybot/Desktop/slam3d/models/segnet_model_driving_webdemo.prototxt";
-    //std::string trained_fil
+    //std::string trained_file = "/home/relaybot/Desktop/slam3d/models/segnet_weights_driving_webdemo.caffemodel";
+    //std::string label_file = "/home/relaybot/Desktop/slam3d/models/semantic12.txt";
+    std::string model_fi
