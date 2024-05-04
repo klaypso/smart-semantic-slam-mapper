db51a8cdd2363c4a80247cdbc3d633c245c23ed5
klaypso <klaypso@proton.me>
2024-05-04 12:18:40 +0000

    Palete/Gir.vala

--- src/segnet.cpp
+++ src/segnet.cpp
@@ -6,4 +6,10 @@ Classifier::Classifier()
 {
 
 #ifdef CPU_ONLY
-	Caffe::set_mode(Caffe::C
+	Caffe::set_mode(Caffe::CPU);
+#else
+	Caffe::set_mode(Caffe::GPU);
+#endif
+
+    //std::string model_file = "/home/relaybot/Desktop/slam3d/models/segnet_model_driving_webdemo.prototxt";
+    //std::string trained_fil
