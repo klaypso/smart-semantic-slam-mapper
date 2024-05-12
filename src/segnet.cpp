31501be64ac5b9da28e4db8eacf9b9b5533b06d7
klaypso <klaypso@proton.me>
2024-05-12 08:13:12 +0000

    Machine Info

--- src/segnet.cpp
+++ src/segnet.cpp
@@ -155,4 +155,9 @@ void Classifier::Preprocess(const cv::Mat& img, std::vector<cv::Mat>* input_chan
 		sample_resized.convertTo(sample_float, CV_32FC1);
 
 	cv::Mat sample_normalized;
-	cv::subtract(sample_float, mean_, sample
+	cv::subtract(sample_float, mean_, sample_normalized);
+
+	/* This operation will write the separate BGR planes directly to the
+	* input layer of the network because it is wrapped by the cv::Mat
+	* objects in input_channels. */
+	cv::split(sample_normal
