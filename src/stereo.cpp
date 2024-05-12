9c6ce188370e15bef297c6d26c5a4f74f5cc608e
klaypso <klaypso@proton.me>
2024-05-12 16:05:00 +0000

    Fixes #12

--- src/stereo.cpp
+++ src/stereo.cpp
@@ -5,4 +5,12 @@ using namespace cv;
 using namespace std;
 
 /*---------  Calculate the disparity may by SGBM algorithm
-* img_L, img_R ----------- Rectified left and right 
+* img_L, img_R ----------- Rectified left and right image 
+* disp         ----------- The disparity result
+*/
+void calDisparity_SGBM(const cv::Mat& img_L, const cv::Mat& img_R, cv::Mat& disp)
+{
+	cv::StereoSGBM sgbm;
+
+	// set the parameters of sgbm
+	int cn = 1; //number of channels
