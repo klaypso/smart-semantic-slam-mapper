66e06493a04ede4946b23bcea6862d3971372278
klaypso <klaypso@proton.me>
2024-05-13 13:29:18 +0000

    Hotfixes

--- src/stereo.cpp
+++ src/stereo.cpp
@@ -15,4 +15,8 @@ void calDisparity_SGBM(const cv::Mat& img_L, const cv::Mat& img_R, cv::Mat& disp
 	// set the parameters of sgbm
 	int cn = 1; //number of channels
 	int SADWindowSize = 11; //Matched block size. It must be an odd number >=1 . Normally, it should be somewhere in the 3..11 range.
-	int numberOfDisparities = 80; //Maximum disparity minus minimum disp
+	int numberOfDisparities = 80; //Maximum disparity minus minimum disparity, must be n*16
+	sgbm.minDisparity = 0; //Minimum possible disparity value
+	sgbm.uniquenessRatio = 10;
+	sgbm.speckleWindowSize = 100;
+	sgbm.preFilterCap = 63; //Truncatio
