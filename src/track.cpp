ea82e016b95a6de5d8072e66bd11b395b9235595
klaypso <klaypso@proton.me>
2024-05-23 22:14:20 +0000

    Build status

--- src/track.cpp
+++ src/track.cpp
@@ -16,4 +16,11 @@ Eigen::Isometry3d Tracker::updateFrame( RGBDFrame::Ptr& newFrame )
     }
     if ( state == OK )
     {
-	    e
+	    estimateVO();//在slam3d的代码里使用的是这个函数，下面的被注释
+        //trackRefFrame();//下面的trackrefframe来自于高博的RGBDFrame跟踪
+        return currentFrame->getTransform();
+    }
+    // state = LOST
+
+    lostRecover();
+    return currentFrame->get
