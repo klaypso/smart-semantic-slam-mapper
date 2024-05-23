e82c720a29709dfd431842ad8b9ea64f7d9841e0
klaypso <klaypso@proton.me>
2024-05-23 09:54:15 +0000

    tracing: Remove most or all of stack tracer stack size from stack_max_size

--- src/track.cpp
+++ src/track.cpp
@@ -7,4 +7,13 @@ using namespace rgbd_tutor;
 
 Eigen::Isometry3d Tracker::updateFrame( RGBDFrame::Ptr& newFrame )
 {
-    unique_lock<mu
+    unique_lock<mutex> lck(adjustMutex);
+    currentFrame = newFrame;
+    if ( state == NOT_READY )
+    {
+        initFirstFrame( );
+        return Eigen::Isometry3d::Identity();
+    }
+    if ( state == OK )
+    {
+	    e
