af1c5ac7a62b33692c82136e49debea10a90e95b
klaypso <klaypso@proton.me>
2024-05-23 06:56:46 +0000

    Backup commit.

--- src/track.cpp
+++ src/track.cpp
@@ -1,1 +1,10 @@
-#
+#include "track.h"
+#include <unistd.h>
+#include "readFile.h"
+
+// tracker的主线程
+using namespace rgbd_tutor;
+
+Eigen::Isometry3d Tracker::updateFrame( RGBDFrame::Ptr& newFrame )
+{
+    unique_lock<mu
