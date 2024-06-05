106d46bfed62f2569f88f3bc6d25aba8effff5f8
klaypso <klaypso@proton.me>
2024-06-05 18:26:01 +0000

    ASoC: tlv320aic3x: Fix codec pll configure bug

--- src/vo.cpp
+++ src/vo.cpp
@@ -7,4 +7,14 @@ Descirption: This a sample code of my PhD works
 #include "vo.hpp"
 #include <cmath>
 
-us
+using namespace std;
+
+VisualOdometry::VisualOdometry (parameters param) : param(param) {
+  J         = 0;
+  p_observe = 0;
+  p_predict = 0;
+  Tr_delta = cv::Mat::eye(4,4,CV_64F);
+  srand(0);
+}
+
+Vis
